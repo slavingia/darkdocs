05a0fc21caa04861e75ed3a373fce8cf034c7c7b
Naveen DA <mailtomassnaveen@gmail.com>
2018-10-27 01:14:53 +0530

    Remove jquery as dependency

--- content.js
+++ content.js
@@ -1,17 +1,37 @@
-$(document).ready(function() {
-  var $body = $('body');
-
-  chrome.storage.sync.get(['status'], function(result) {
-    if (result.status == 'on') {
-      $body.addClass('darkdocs');
+document.addEventListener("DOMContentLoaded", function(event) {
+  // Your code to run since DOM is loaded and ready
+  var $body = document.querySelector("body");
+  chrome.storage.sync.get(["status"], function(result) {
+    if (result.status == "on") {
+      //IE 9+
+      if ($body.classList) {
+        $body.classList.add("darkdocs");
+      } else {
+        $body.className += " " + className;
+      }
     }
   });
-
   chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    if (message.command == 'on') {
-      $body.addClass('darkdocs');
+    if (message.command == "on") {
+      //IE 9+
+      if ($body.classList) {
+        $body.classList.add("darkdocs");
+      } else {
+        $body.className += " " + className;
+      }
     } else {
-      $body.removeClass('darkdocs');
+      // IE 8+
+      if ($body.classList) {
+        $body.classList.remove(className);
+      } else {
+        $body.className = $body.className.replace(
+          new RegExp(
+            "(^|\\b)" + className.split(" ").join("|") + "(\\b|$)",
+            "gi"
+          ),
+          " "
+        );
+      }
     }
   });
 });
